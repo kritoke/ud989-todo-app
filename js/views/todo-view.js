637a14121a8e5e460a9dfd01f3b9a05029adefd0
Matt Rhone <kritoke@gmail.com>
2017-11-23 17:33:45 -0600

    Added priority todo items

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -21,6 +21,7 @@ var app = app || {};
             'dblclick label': 'edit',
             'click .edit-btn': 'edit',
             'click .destroy': 'clear',
+            'click .priority-btn': 'togglePriority',
             'keypress .edit': 'updateOnEnter',
             'keydown .edit': 'revertOnEscape',
             'blur .edit': 'close'
@@ -51,6 +52,7 @@ var app = app || {};
 
             this.$el.html(this.template(this.model.toJSON()));
             this.$el.toggleClass('completed', this.model.get('completed'));
+            this.$el.toggleClass('priority', this.model.get('priority'));
             this.toggleVisible();
             this.$input = this.$('.edit');
             return this;
@@ -71,11 +73,17 @@ var app = app || {};
             this.model.toggle();
         },
 
+        // Toggle the `"priority"` state of the model.
+        togglePriority: function() {
+            this.model.togglePriority();
+        },
+
         // Switch this view into `"editing"` mode, displaying the input field.
         edit: function() {
             this.$el.addClass('editing');
             this.$input.focus();
         },
+
 
         // Close the `"editing"` mode, saving changes to the todo.
         close: function() {
